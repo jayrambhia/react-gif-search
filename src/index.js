fea1771de36817bb35a331715abb7868473d32fa
Jay Rambhia <jay.rambhia@booking.com>
2017-04-19 16:37:18 +0200

    Redux added
    Signed-off-by: Jay Rambhia <jay.rambhia@booking.com>

--- src/index.js
+++ src/index.js
@@ -1,59 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
-import GifList from './components/GifList';
-import GifModal from './components/GifModal';
-import request from 'superagent';
-import './styles/app.css';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-class App extends React.Component {
+const store = configureStore();
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      gifs: [],
-      term: '',
-      selectedGif: null,
-      modalIsOpen: false
-    };
-
-    this.handleTermChange = this.handleTermChange.bind(this);
-  }
-
-  handleTermChange(term) {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
-    request.get(url, (err, res) => {
-      this.setState({ gifs: res.body.data });
-    });
-
-    this.setState({ term:term });
-  }
-
-  openModal(gif) {
-    this.setState({
-      modalIsOpen: true,
-      selectedGif: gif
-    });
-  }
-
-  closeModal() {
-    this.setState({
-      modalIsOpen: false,
-      selectedGif: null
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar onTermChange={term => this.handleTermChange(term)} />
-        <GifList gifs={this.state.gifs} onGifSelect={selectedGif => this.openModal(selectedGif)} />
-        <GifModal modalIsOpen={this.state.modalIsOpen}
-                  selectedGif={this.state.selectedGif}
-                  onRequestClose={ () => this.closeModal() } />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ src/store/configureStore.js
@@ -0,0 +1,25 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers (
+      applyMiddleware(ReduxPromise)
+    )
+  );
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers').default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+}
