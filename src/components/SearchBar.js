5f70c5fc6373c5d2ff64317746e761aff46023a6
Jay Rambhia <jay.rambhia@booking.com>
2017-04-21 14:02:00 +0200

    RxJs added.
    Using RxJs debounce to wait for user input.
    
    Signed-off-by: Jay Rambhia <jay.rambhia@booking.com>

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Rx from 'rxjs/Rx';
 
 class SearchBar extends React.Component {
 
-  onInputChange(term) {
+  componentDidMount() {
+    this.subject = new Rx.Subject();
+    this.subject.debounceTime(800)
+                .filter(term => term.trim().length > 0)
+                .subscribe(term => this.onTermChange(term.trim()));
+  }
+
+  onTermChange(term) {
+    console.log("onTermChange: " + term);
     this.props.onTermChange(term);
+  }
+
+  onInputChange(term) {
+    this.subject.next(term);
   }
 
   render() {
@@ -12,6 +25,14 @@ class SearchBar extends React.Component {
         <input placeholder="Find your gifs here!" onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  componentWillUnmount() {
+    console.log("unmount");
+    if (this.subject !== undefined) {
+      console.log("complete");
+      this.subject.complete();
+    }
   }
 }
 
